d4cb375a4b14e9d2fb3e29a5e763198cd8b98ebc
James L <59751464+Jazz1016@users.noreply.github.com>
2021-09-11 16:57:38 -0600

    hit a roadblock going to come back to it tomorrow

--- GoogleSignInTestApp/GoogleSignInTestApp/AppDelegate.swift
+++ GoogleSignInTestApp/GoogleSignInTestApp/AppDelegate.swift
@@ -5,16 +5,25 @@
 //  Created by James Lea on 9/10/21.
 //
 
+import GoogleSignIn
+import Firebase
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
+    }
+    
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any])
+    -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
     }
 
     // MARK: UISceneSession Lifecycle
